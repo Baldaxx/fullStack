301df0af5b0b373c9bca1c02a96306f9a107aae1
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-02-08 17:26:57 +0100

    addButtonFront

---
+++ public/script.js
@@ -0,0 +1,4 @@
+const addArticleButton = document.getElementById('add-article-button');
+addArticleButton.addEventListener('click', () => {
+    alert('Nous nous efforcons de rendre le site fonctionnel, nous ne sommes pas des machines !!!');
+});
